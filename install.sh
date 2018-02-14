643883e8a44bab73dfd4bf8fd3da39fb91fb4684
Fabio <miranda.fmm@gmail.com>
2018-02-14 01:03:43 -0300

    Updated README and install.sh to make installation easier

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -x
-workdir=$(pwd)
+WORKDIR=$(pwd)
 make
-echo 'export PATH='$workdir'/bin/Debug:$PATH' >> ~/.bashrc
+echo 'export PATH='$WORKDIR'/bin/Debug:$PATH' >> ~/.bashrc
 wget http://cab.spbu.ru/files/release3.11.1/SPAdes-3.11.1-Linux.tar.gz
 tar -xvzf SPAdes-3.11.1-Linux.tar.gz
-echo 'export PATH='$workdir'/SPAdes-3.11.1-Linux/bin:$PATH' >> ~/.bashrc
+echo 'export PATH='$WORKDIR'/SPAdes-3.11.1-Linux/bin:$PATH' >> ~/.bashrc
 git clone https://github.com/pmelsted/KmerStream.git
 cd KmerStream
 make
-echo 'export PATH='$workdir'/KmerStream:$PATH' >> ~/.bashrc
+echo 'export PATH='$WORKDIR'/KmerStream:$PATH' >> ~/.bashrc
 source ~/.bashrc
